4ab6c280b40d2726c8b83f1405bb0155a5723317
jonghun in Win10 <jonghun.criss.android@gmail.com>
2024-05-13 22:35:55 +0900

    initial

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AndroidLab"
+include(":app")
+include(":ch6_view")
+include(":ch8_event")
+include(":ch9_resource")
+include(":ch10_notification")
