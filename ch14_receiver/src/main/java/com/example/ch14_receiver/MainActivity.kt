7939b1556e9e01d79515f92101fb7414a035b790
jonghun in Win10 <jonghun.criss.android@gmail.com>
2024-06-26 22:44:20 +0900

    add ch14

---
+++ ch14_receiver/src/main/java/com/example/ch14_receiver/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.ch14_receiver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ch14_receiver.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //add......................
+
+    }
+}
