d5b335bc063211bce750145d91ed5a85583a61db
jonghun in Win10 <jonghun.criss.android@gmail.com>
2024-09-01 22:13:18 +0900

    add ch17

---
+++ ch17_database/src/main/java/com/example/ch17_database/MySettingFragment.kt
@@ -0,0 +1,31 @@
+package com.example.ch17_database
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.preference.EditTextPreference
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+
+
+class MySettingFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+
+        val idPreference: EditTextPreference? = findPreference("id")
+        val colorPreference: ListPreference? = findPreference("color")
+
+        colorPreference?.summaryProvider = ListPreference.SimpleSummaryProvider.getInstance()
+
+        idPreference?.summaryProvider =
+            Preference.SummaryProvider<EditTextPreference> {
+                val text = it.text
+                if (TextUtils.isEmpty(text)) {
+                    "설정이 되지 않았습니다."
+                } else {
+                    "설정된 id 값은 $text 입니다."
+                }
+            }
+    }
+
+}
